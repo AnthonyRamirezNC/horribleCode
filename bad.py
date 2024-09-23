c4a67392df8cd17fc29ea60f3e0eed3f60f67d33
moeezawann <moeez.awan15@gmail.com>
2024-09-23 15:48:06 -0400

    first function KISS

---
+++ bad.py
@@ -0,0 +1,24 @@
+class Calcualtor:
+    def __init__(self,num1,num2):
+        self.num1 = num1
+        self.num2 = num2
+    
+    # funciton to add numbers 
+    def adding_numbers(num1,num2):
+        #using a stack because it is OPTIMALLLLLL
+        stack = []
+        addition = 0  
+        stack.append(num1)
+        stack.append(num2)
+
+        for i in stack:
+             addition += i
+        return addition
+        
+    
+    #def Mutliplying 
+
+
+calc = Calcualtor
+print(calc.adding_numbers(1,4))
+    
